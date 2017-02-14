58c989ac81b30778f2e20a8520e61af41bbe9388
Taylor Scott <TaylorScott@Taylors-MacBook-Pro.local>
2017-02-14 16:20:05 -0500

    mvp complete i think; got sound working

--- app.rb
+++ app.rb
@@ -11,5 +11,37 @@ get '/' do
 end
 
 get '/pokemons' do
-  "Hello world"
+  @pokemons = Pokemon.all
+  erb :"pokemons/index"
+end
+
+get '/pokemons/new' do
+  erb :"pokemons/new"
+end
+
+get '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemons/show"
+end
+
+post '/pokemons' do
+  @pokemon = Pokemon.create(params[:pokemon])
+  redirect "/pokemons/#{@pokemon.id}"
+end
+
+get "/pokemons/:id/edit" do
+  @pokemon = Pokemon.find(params[:id])
+  erb(:"pokemons/edit")
+end
+
+put '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:pokemon])
+  redirect("/pokemons/#{@pokemon.id}")
+end
+
+delete '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.destroy
+  redirect("/pokemons")
 end
